f7d3fc2a9072f8df4a5a4af86231f1732e5ddd60
amazingz <amazingtr@gmai.com>
2020-02-29 23:07:48 +0200

    use image in carduser

--- src/components/User.js
+++ src/components/User.js
@@ -6,8 +6,9 @@ export default class User extends Component {
             <div className="usercard">
                 {/* <img src={this.props.src} alt={this.props.alt}> </img> */}
 
-                <div className="post__name">
+                <div className="user__name">
                     <div>UserCard</div>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNbDi-0nqUAYQlVYj6e4YVxQ5cORbmT3gruyc7_1KFtx6DzknX" alt="user"></img>
                 </div>
             </div>
         )
